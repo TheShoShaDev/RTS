029fe3701d452a1041095bef1dc79d9122ee4ac4
The ShoSha <Ratattui007@mail.ru>
2024-08-04 22:00:01 +0700

    еще тесты

--- Assets/_Project/Scripts/Entities/Fanatics/Units/FootMan.cs
+++ Assets/_Project/Scripts/Entities/Fanatics/Units/FootMan.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Entity : MonoBehaviour
+public class FootMan : MonoBehaviour
 {
-    [SerializeField] Health _health;
-
-
     // Start is called before the first frame update
     void Start()
     {
